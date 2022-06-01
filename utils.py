14867f455084fae7a53ff0a26c5a464acf3f99c6
Michal Gulczynski <malygulczas@gmail.com>
2022-06-01 18:35:48 +0200

    utils 1.0

---
+++ utils.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import torch
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    for epoch in range(epochs):
+        # Training Phase
+        model.train()
+        train_losses = []
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
