aaff3ed58bc5205fae41803d68a49cd62cbc37b8
matte <mattemill@protonmail.com>
2022-06-02 00:20:37 +0200

    Images are now getting recognised by a tractor.

--- utils.py
+++ utils.py
@@ -39,3 +39,13 @@ def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
         model.epoch_end(epoch, result)
         history.append(result)
     return history
+
+def predict_image(img, model):
+    # Convert to a batch of 1
+    xb = to_device(img.unsqueeze(0), device)
+    # Get predictions from model
+    yb = model(xb)
+    # Pick index with highest probability
+    _, preds = torch.max(yb, dim=1)
+    # Retrieve the class label
+    return dataset.classes[preds[0].item()]
