39d3493fcedfb7afbb5c211a980f62bff994a238
Adam Wieczarek <adawie1@st.amu.edu.pl>
2022-06-02 00:06:58 +0200

    Added class and metods for neutral network

--- utils.py
+++ utils.py
@@ -1,6 +1,26 @@
 import matplotlib.pyplot as plt
 import torch
 
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
 def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
     history = []
     optimizer = opt_func(model.parameters(), lr)
