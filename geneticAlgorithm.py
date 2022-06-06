68ef62566fd0e3ca63871088cbfda25bbbfab3ee
Kamil <kamilspychala2001@gmail.com>
2022-06-06 18:31:27 +0200

    Tworzenie genetycznego algorytmu i funkcji niezbędnych

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -89,3 +89,73 @@ def dfs(tablica, x, y, visited, m, n):
         if match3(tablica, x + dx, y + dy, m, n) and visited[x + dx][y + dy] == 0:
             odw += dfs(tablica, x + dx, y + dy, visited, m, n)
     return odw
+
+
+def mutation(tablica, n):
+    # n to długość boku kwadratu
+    x = random.randint(0, n - 1)
+    y = random.randint(0, n - 1)
+
+    if tablica[x][y] > 3:
+        tablica[x][y] = random.randint(4, 8)
+    else:
+        pass
+    return tablica
+
+
+def crossover(tab1, tab2, n):
+    x = random.randint(-1, n - 1)
+    return tab1[0:x + 1] + tab2[x + 1:n], tab2[0:x + 1] + tab1[x + 1:n]
+
+
+def fitness(tablica, n):  # n - wielkość tablicy
+    x = m_not_around_m2(tablica, 5, 2, n) + \
+        m_not_around_m2(tablica, 6, 8, n) + \
+        m_not_over_edge(tablica, 1, n) + \
+        max_k_vegetables_in_group(tablica, 4, 8, n)
+    return round(x, 3)
+
+
+def create_populate(n, N):  # N - liczba losowych populacji
+    population = [[[0] * n for _ in range(n)] for __ in range(N)]
+    for k in range(N):
+        for i in range(n):
+            for j in range(n):
+                x = random.choices([1, 2, 3, 4, 5, 6, 7, 8], weights=[1, 1, 1, 3, 3, 3, 3, 3], k=1)
+                population[k][i][j] = x[0]
+    return population
+
+
+def how_did_it_go(population, n):
+    result = []
+    for i in population:
+        x = fitness(i, n)
+        result.append(x)
+    return result
+
+
+def selection(population, n):
+    return random.choices(population, weights=how_did_it_go(population, n), k=2)
+
+
+def run_evolution(n, N):  # N jak duża populacja
+    population = create_populate(n, N)
+
+    while N != 2 and N != 1:
+        new_population = []
+        selected_parent1, selected_parent2 = selection(population, 5)
+        new_population.append(selected_parent1)
+        new_population.append(selected_parent2)
+
+        for j in range(int((N - 4) / 2)):
+            cross_parent1, cross_parent2 = crossover(selected_parent1, selected_parent2, n)
+            cross_parent1_mutation = mutation(cross_parent1, n)
+            cross_parent2_mutation = mutation(cross_parent2, n)
+
+            new_population.append(cross_parent1_mutation)
+            new_population.append(cross_parent2_mutation)
+
+        population = new_population
+        N -= 2
+    return max(how_did_it_go(population, n)), population[0]
+
